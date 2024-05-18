978e5d2c4357d62e1e442fe786fba289b4bce501
ranjhal23 <ranjhal2003@gmail.com>
2024-05-18 18:25:43 +0530

    started

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose= require("mongoose")
+require("dotenv").config()
+const connectDb=()=>{
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology:true,                               
+    })
+    .then(console.log("Db successfull"))
+    .catch((e)=>{
+        console.log("DB error")
+        console.log(e)
+        process.exit(1)
+    })
+}
+module.exports=connectDb
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express= require("express")
 const app= express()
-app.listen()
+require("dotenv").config()
+const PORT= process.env.PORT||3000
+app.use(express.json())
+const blog=require("./routes/blog")
+app.use("/api/v1", blog)
+const connectDB= require("./config/database")
+connectDB()
+app.listen(PORT,()=>{
+    console.log(`App started at ${PORT}`)
+})
+app.get("/", (req, res)=>{
+    res.send(`<h1>This is home page</h1>`)
+})
